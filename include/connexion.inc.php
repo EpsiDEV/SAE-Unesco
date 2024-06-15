6a2f692488391b0106b9f0f2d0859cb7d09bfdfa
Jawed <148317935+Wedja10@users.noreply.github.com>
2024-06-15 12:46:36 +0100

    Update connexion.inc.php

--- include/connexion.inc.php
+++ include/connexion.inc.php
@@ -1,10 +1,10 @@
 <?php
-$user = 'jawed.essahli';
-$pass = 'Ermite';
+$user = '';
+$pass = '';
 try {
-    $bdd = new PDO('pgsql:host=sqletud.u-pem.fr; dbname=jawed.essahli_db', $user, $pass);
+    $bdd = new PDO('pgsql:host=sqletud.u-pem.fr; dbname=', $user, $pass);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Activer le mode d'erreur
-    $bdd->exec("SET search_path TO unesco;");
+    $bdd->exec("");
 } catch (PDOException $e) {
     echo "ERREUR : La connexion a échouée : " . $e->getMessage();
     die();
